21f5c81497a26a1ddae17438f86c4d10456a2613
Colton Berger <coltonberger@optonline.net>
2018-05-09 16:21:51 -0700

    updated structure

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 window.onload = function() {
-  document.getElementsByClassName("col-lg-4 col-sm-6 text-center mb-4").addEventListener("click", function(){
-    console.log("clicked")
-});
+  let selected = document.querySelectorAll("li");
+  for (let i = 0; i < selected.length; i++){
+    let clicked = selected[i];
+    clicked.addEventListener("click", namedFunction);
+  }
+  function namedFunction(event){
+  let elem = document.querySelector("class")
+    if(elem) {
+      elem.className = "";
+    }
+      this.className = "selected"
 
-
-
-
-
-  // let allListItems = document.querySelectorAll("col-lg-4 col-sm-6 text-center mb-4");
-  // allListItems.forEach((listItem) =>{
-  //   listItem.addEventListener("click", namedFunction);
-  //   console.log("click")
-  // })
-  // function namedFunction(event){
-  // let elem = document.querySelector(".selected")
-  //   if(elem) {
-  //     elem.className = "";
-  //   }
-  //     this.className = "selected"
-  // }
+  }
 }
