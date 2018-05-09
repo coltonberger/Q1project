26138b9f758096c477f9553e6d16bf6560a76955
Colton Berger <coltonberger@optonline.net>
2018-05-09 15:36:50 -0700

    added js file

---
+++ index.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+  document.getElementsByClassName("col-lg-4 col-sm-6 text-center mb-4").addEventListener("click", function(){
+    console.log("clicked")
+});
+
+
+
+
+
+  // let allListItems = document.querySelectorAll("col-lg-4 col-sm-6 text-center mb-4");
+  // allListItems.forEach((listItem) =>{
+  //   listItem.addEventListener("click", namedFunction);
+  //   console.log("click")
+  // })
+  // function namedFunction(event){
+  // let elem = document.querySelector(".selected")
+  //   if(elem) {
+  //     elem.className = "";
+  //   }
+  //     this.className = "selected"
+  // }
+}
