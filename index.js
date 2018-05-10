54a040d7dc45654fa3bab646ce74cc3f220168a5
Colton Berger <coltonberger@optonline.net>
2018-05-10 11:20:21 -0700

    click image with border

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 window.onload = function() {
-  let selected = document.querySelectorAll("li");
-  for (let i = 0; i < selected.length; i++){
-    let clicked = selected[i];
-    clicked.addEventListener("click", namedFunction);
-  }
-  function namedFunction(event){
-  let elem = document.querySelector("class")
-    if(elem) {
-      elem.className = "";
-    }
-      this.className = "selected"
+  let allListItems = document.querySelectorAll("li");
+  console.log(allListItems)
+  allListItems.forEach((listItem) =>{
+    listItem.addEventListener("click", namedFunction);
+  });
 
-  }
+    function namedFunction(event){
+      let elem = document.querySelector(".selected")
+      if(elem) {
+      elem.className = "";
+      }
+      this.className = "selected"
+      }
 }
